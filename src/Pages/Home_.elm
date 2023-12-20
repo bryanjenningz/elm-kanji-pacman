e6c6970882d93a1a7b15642effb06be482914986
Bryan Jennings <bryanjenningz@gmail.com>
2023-12-20 06:14:16 -0800

    Run `elm-land add page /`

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,70 @@
-module Pages.Home_ exposing (page)
+module Pages.Home_ exposing (Model, Msg, page)
 
+import Effect exposing (Effect)
+import Route exposing (Route)
 import Html
+import Page exposing (Page)
+import Shared
 import View exposing (View)
 
 
-page : View msg
-page =
-    { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+page : Shared.Model -> Route () -> Page Model Msg
+page shared route =
+    Page.new
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Effect Msg )
+init () =
+    ( {}
+    , Effect.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model
+            , Effect.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    { title = "Pages.Home_"
+    , body = [ Html.text "/" ]
     }
