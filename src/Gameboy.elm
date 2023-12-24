5167f815173679b6929fd20c75264b5df29dfe7a
Bryan Jennings <bryanjenningz@gmail.com>
2023-12-23 21:27:56 -0800

    Break up gameboy view into multiple functions

--- src/Gameboy.elm
+++ src/Gameboy.elm
@@ -143,195 +143,210 @@ view controls screen =
         , style "flex-direction" "column"
         , style "align-items" "center"
         ]
-        -- GAMEBOY TOP
-        [ div
-            [ style "width" "280px"
-            , style "height" "230px"
-            , style "background-color" "black"
-            , style "border-radius" "16px"
-            , style "display" "flex"
-            , style "justify-content" "center"
-            , style "align-items" "center"
-            ]
-            -- GAMEBOY SCREEN
-            [ div
-                [ style "width" "200px"
-                , style "height" "200px"
-                , style "background-color" "#333"
-                , style "border-radius" "16px"
-                , style "position" "relative"
-                , style "overflow" "hidden"
-                ]
-                [ screen ]
-            ]
+        [ viewTop screen
+        , viewBottom controls
+        ]
 
-        -- GAMEBOY BOTTOM
-        , div
+
+viewTop : Html msg -> Html msg
+viewTop screen =
+    div
+        [ style "width" "280px"
+        , style "height" "230px"
+        , style "background-color" "black"
+        , style "border-radius" "16px"
+        , style "display" "flex"
+        , style "justify-content" "center"
+        , style "align-items" "center"
+        ]
+        [ div
+            [ style "width" "200px"
+            , style "height" "200px"
+            , style "background-color" "#333"
+            , style "border-radius" "16px"
+            , style "position" "relative"
+            , style "overflow" "hidden"
+            ]
+            [ screen ]
+        ]
+
+
+viewBottom : Controls msg -> Html msg
+viewBottom controls =
+    div
+        [ style "display" "flex"
+        , style "flex-direction" "column"
+        , style "gap" "16px"
+        , style "width" "100%"
+        ]
+        [ div
             [ style "display" "flex"
-            , style "flex-direction" "column"
-            , style "gap" "16px"
+            , style "justify-content" "space-between"
+            , style "padding" "32px 0"
             , style "width" "100%"
             ]
-            -- GAMEBOY MAIN CONTROLS
-            [ div
-                [ style "display" "flex"
-                , style "justify-content" "space-between"
-                , style "padding" "32px 0"
-                , style "width" "100%"
-                ]
-                -- GAMEBOY LEFT PAD
-                [ div
-                    [ style "position" "relative"
-                    , style "width" "100px"
-                    , style "height" "100px"
-                    ]
-                    [ div
-                        [ style "position" "absolute"
-                        , style "top" "30px"
-                        , style "width" "100px"
-                        , style "height" "40px"
-                        , style "background-color" "black"
-                        , style "border-radius" "99px"
-                        , style "overflow" "hidden"
-                        ]
-                        [ button
-                            [ style "position" "absolute"
-                            , style "top" "0"
-                            , style "bottom" "0"
-                            , style "left" "0"
-                            , style "width" "40px"
-                            , onMouseDown (controls.onPadDown Left)
-                            , onMouseUp (controls.onPadUp Left)
-                            ]
-                            []
-                        , button
-                            [ style "position" "absolute"
-                            , style "top" "0"
-                            , style "bottom" "0"
-                            , style "right" "0"
-                            , style "width" "40px"
-                            , onMouseDown (controls.onPadDown Right)
-                            , onMouseUp (controls.onPadUp Right)
-                            ]
-                            []
-                        ]
-                    , div
-                        [ style "position" "absolute"
-                        , style "left" "30px"
-                        , style "width" "40px"
-                        , style "height" "100px"
-                        , style "background-color" "black"
-                        , style "border-radius" "99px"
-                        , style "overflow" "hidden"
-                        ]
-                        [ button
-                            [ style "position" "absolute"
-                            , style "top" "0"
-                            , style "left" "0"
-                            , style "right" "0"
-                            , style "height" "40px"
-                            , onMouseDown (controls.onPadDown Up)
-                            , onMouseUp (controls.onPadUp Up)
-                            ]
-                            []
-                        , button
-                            [ style "position" "absolute"
-                            , style "bottom" "0"
-                            , style "left" "0"
-                            , style "right" "0"
-                            , style "height" "40px"
-                            , onMouseDown (controls.onPadDown Down)
-                            , onMouseUp (controls.onPadUp Down)
-                            ]
-                            []
-                        ]
-
-                    -- MOBILE TOUCH PAD OVERLAY
-                    -- We need to use a single button instead of 4 buttons so
-                    -- that we can handle touchmove events that start in one
-                    -- direction and change to a different direction
-                    -- (e.g. the user touches the UP button then slides their
-                    -- finger to the LEFT button without lifting their finger)
-                    , button
-                        [ style "position" "absolute"
-                        , style "inset" "0"
-                        , style "z-index" "1"
-                        , onTouchStart
-                            (\maybeDirection ->
-                                case maybeDirection of
-                                    Nothing ->
-                                        controls.onAllPadsUp
-
-                                    Just direction ->
-                                        controls.onPadDown direction
-                            )
-                        , onTouchMove
-                            (\maybeDirection ->
-                                case maybeDirection of
-                                    Nothing ->
-                                        controls.onAllPadsUp
-
-                                    Just direction ->
-                                        controls.onPadDown direction
-                            )
-                        , onTouchEnd controls.onAllPadsUp
-                        ]
-                        []
-                    ]
-
-                -- GAMEBOY RIGHT BUTTONS
-                , div
-                    [ style "position" "relative"
-                    , style "width" "100px"
-                    , style "height" "100px"
-                    ]
-                    [ button
-                        [ style "position" "absolute"
-                        , style "top" "45px"
-                        , style "width" "50px"
-                        , style "height" "50px"
-                        , style "background-color" "black"
-                        , style "border-radius" "99px"
-                        , onClick controls.onClickB
-                        ]
-                        []
-                    , button
-                        [ style "position" "absolute"
-                        , style "top" "10px"
-                        , style "right" "0px"
-                        , style "width" "50px"
-                        , style "height" "50px"
-                        , style "background-color" "black"
-                        , style "border-radius" "99px"
-                        , onClick controls.onClickA
-                        ]
-                        []
-                    ]
-                ]
-
-            -- GAMEBOY START / SELECT
-            , div
-                [ style "width" "100%"
-                , style "display" "flex"
-                , style "justify-content" "center"
-                , style "gap" "20px"
-                ]
-                [ button
-                    [ style "width" "40px"
-                    , style "height" "15px"
-                    , style "background-color" "black"
-                    , style "border-radius" "20px"
-                    , onClick controls.onClickSelect
-                    ]
-                    []
-                , button
-                    [ style "width" "40px"
-                    , style "height" "15px"
-                    , style "background-color" "black"
-                    , style "border-radius" "20px"
-                    , onClick controls.onClickStart
-                    ]
-                    []
-                ]
+            [ viewDirectionPad controls
+            , viewABButtons controls
             ]
+        , viewStartSelectButtons controls
+        ]
+
+
+viewDirectionPad : Controls msg -> Html msg
+viewDirectionPad controls =
+    div
+        [ style "position" "relative"
+        , style "width" "100px"
+        , style "height" "100px"
+        ]
+        [ div
+            [ style "position" "absolute"
+            , style "top" "30px"
+            , style "width" "100px"
+            , style "height" "40px"
+            , style "background-color" "black"
+            , style "border-radius" "99px"
+            , style "overflow" "hidden"
+            ]
+            [ button
+                [ style "position" "absolute"
+                , style "top" "0"
+                , style "bottom" "0"
+                , style "left" "0"
+                , style "width" "40px"
+                , onMouseDown (controls.onPadDown Left)
+                , onMouseUp (controls.onPadUp Left)
+                ]
+                []
+            , button
+                [ style "position" "absolute"
+                , style "top" "0"
+                , style "bottom" "0"
+                , style "right" "0"
+                , style "width" "40px"
+                , onMouseDown (controls.onPadDown Right)
+                , onMouseUp (controls.onPadUp Right)
+                ]
+                []
+            ]
+        , div
+            [ style "position" "absolute"
+            , style "left" "30px"
+            , style "width" "40px"
+            , style "height" "100px"
+            , style "background-color" "black"
+            , style "border-radius" "99px"
+            , style "overflow" "hidden"
+            ]
+            [ button
+                [ style "position" "absolute"
+                , style "top" "0"
+                , style "left" "0"
+                , style "right" "0"
+                , style "height" "40px"
+                , onMouseDown (controls.onPadDown Up)
+                , onMouseUp (controls.onPadUp Up)
+                ]
+                []
+            , button
+                [ style "position" "absolute"
+                , style "bottom" "0"
+                , style "left" "0"
+                , style "right" "0"
+                , style "height" "40px"
+                , onMouseDown (controls.onPadDown Down)
+                , onMouseUp (controls.onPadUp Down)
+                ]
+                []
+            ]
+
+        -- MOBILE TOUCH PAD OVERLAY
+        -- We need to use a single button instead of 4 buttons so
+        -- that we can handle touchmove events that start in one
+        -- direction and change to a different direction
+        -- (e.g. the user touches the UP button then slides their
+        -- finger to the LEFT button without lifting their finger)
+        , button
+            [ style "position" "absolute"
+            , style "inset" "0"
+            , style "z-index" "1"
+            , onTouchStart
+                (\maybeDirection ->
+                    case maybeDirection of
+                        Nothing ->
+                            controls.onAllPadsUp
+
+                        Just direction ->
+                            controls.onPadDown direction
+                )
+            , onTouchMove
+                (\maybeDirection ->
+                    case maybeDirection of
+                        Nothing ->
+                            controls.onAllPadsUp
+
+                        Just direction ->
+                            controls.onPadDown direction
+                )
+            , onTouchEnd controls.onAllPadsUp
+            ]
+            []
+        ]
+
+
+viewABButtons : Controls msg -> Html msg
+viewABButtons controls =
+    div
+        [ style "position" "relative"
+        , style "width" "100px"
+        , style "height" "100px"
+        ]
+        [ button
+            [ style "position" "absolute"
+            , style "top" "45px"
+            , style "width" "50px"
+            , style "height" "50px"
+            , style "background-color" "black"
+            , style "border-radius" "99px"
+            , onClick controls.onClickB
+            ]
+            []
+        , button
+            [ style "position" "absolute"
+            , style "top" "10px"
+            , style "right" "0px"
+            , style "width" "50px"
+            , style "height" "50px"
+            , style "background-color" "black"
+            , style "border-radius" "99px"
+            , onClick controls.onClickA
+            ]
+            []
+        ]
+
+
+viewStartSelectButtons : Controls msg -> Html msg
+viewStartSelectButtons controls =
+    div
+        [ style "width" "100%"
+        , style "display" "flex"
+        , style "justify-content" "center"
+        , style "gap" "20px"
+        ]
+        [ button
+            [ style "width" "40px"
+            , style "height" "15px"
+            , style "background-color" "black"
+            , style "border-radius" "20px"
+            , onClick controls.onClickSelect
+            ]
+            []
+        , button
+            [ style "width" "40px"
+            , style "height" "15px"
+            , style "background-color" "black"
+            , style "border-radius" "20px"
+            , onClick controls.onClickStart
+            ]
+            []
         ]
